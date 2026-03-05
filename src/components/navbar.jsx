02f3ca104c31b638076a4eb899ca650ce9d18488
Agus <agustin.barrios.berra@gmail.com>
2026-03-05 16:49:58 -0300

    pfp and hamburguer error

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,6 +10,7 @@ export default function Navbar() {
                 {isOpen ? '✕' : '☰'}
             </button>
             <ul className={isOpen ? 'open' : ''}>
+                <li className="pfp-item"><a href="https://github.com/0xlibless" target="_blank" rel="noopener noreferrer"><img src="https://github.com/0xlibless.png" alt="0xlibless pfp" className="pfp" /></a></li>
                 <li><a href="#inicio"    onClick={() => setIsOpen(false)}>Inicio</a></li>
                 <li><a href="#sobre-mi"  onClick={() => setIsOpen(false)}>Sobre mi</a></li>
                 <li><a href="#proyectos" onClick={() => setIsOpen(false)}>Proyectos</a></li>
